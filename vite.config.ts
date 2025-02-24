aaba704b96fafcdbc1bfc1f014792d6a1323f668
Iboro Ekanem <eiboro@oinvent.com>
2025-02-24 09:44:55 +0100

    Update path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/fort/", // This ensures assets load correctly in the subdirectory
+  base: "/fort/",
 });
